24c903dd2502c59c2b60c942214806dbbaad11ce
Roman Vanesyan <roman@vanesyan.com>
2024-09-05 12:58:02 +0300

    feat: mb that will work

--- flake.nix
+++ flake.nix
@@ -16,29 +16,30 @@
 
           INPUT_FILE="cv.typ"
           DEFAULT_OUTPUT="cv"
-          DEFAULT_FORMAT="pdf"
 
           function print_usage {
-            echo "Usage: cv [build|watch] [output_name] [format]"
+            echo "Usage: cv [build|watch] [output_name]"
             echo "  build              Compile cv.typ"
             echo "  watch              Watch cv.typ for changes and recompile"
+            echo "  preview            Compile cv.typ to png"
             echo "  output_name        Optional: Specify the output name (default: cv)"
-            echo "  format             Optional: Specify the output format (default: pdf)"
-            echo "                     Supported formats: pdf, png, svg"
           }
 
           COMMAND=$1
           OUTPUT_NAME=''${2:-$DEFAULT_OUTPUT}
-          FORMAT=''${3:-$DEFAULT_FORMAT}
 
           case "$COMMAND" in
             build)
-              echo "Building $INPUT_FILE to $OUTPUT_NAME.$FORMAT..."
-              typst compile $INPUT_FILE "$OUTPUT_NAME.$FORMAT"
+              echo "Building $INPUT_FILE to $OUTPUT_NAME.pdf..."
+              typst compile $INPUT_FILE "$OUTPUT_NAME.pdf" -f pdf
               ;;
             watch)
               echo "Watching $INPUT_FILE for changes and compiling to $OUTPUT_NAME.$FORMAT..."
-              typst watch $INPUT_FILE "$OUTPUT_NAME.$FORMAT"
+              typst watch $INPUT_FILE "$OUTPUT_NAME.pdf" -f pdf
+              ;;
+            preview)
+              echo "Building $INPUT_FILE to ''${OUTPUT_NAME}_{n}.png..."
+              typst compile $INPUT_FILE "''${OUTPUT_NAME}_{n}.png" -f png
               ;;
             *)
               echo "Unknown command: $COMMAND"
@@ -57,11 +58,10 @@
           shellHook = ''
             echo "Welcome to the CV development environment!"
             echo "Available commands:"
-            echo "  cv build [output_name] [format]  - Compile cv.typ"
-            echo "  cv watch [output_name] [format]  - Watch cv.typ for changes and recompile"
-            echo "  output_name                      - Optional: Specify the output name (default: cv)"
-            echo "  format                           - Optional: Specify the output format (default: pdf)"
-            echo "                                     Supported formats: pdf, png, svg"
+            echo "  cv build   [output_name] - Compile cv.typ"
+            echo "  cv watch   [output_name] - Watch cv.typ for changes and recompile"
+            echo "  cv preview [output_name] - Compile cv.typ to preview images"
+            echo "  output_name              - Optional: Specify the output name (default: cv)"
           '';
         };
       }
